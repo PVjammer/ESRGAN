a37fda8d4efe58eff4dc0ce1cffd8ee4051a7871
Nick <nicholas.c.burnett@gmail.com>
2020-05-14 19:18:12 -0400

    Cleanup

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import glob
 import cv2
 import numpy as np
 import torch
-import RRDBNet_arch as arch
+import esrgan.rrdbnet as arch
 
 model_path = 'models/RRDB_ESRGAN_x4.pth'  # models/RRDB_ESRGAN_x4.pth OR models/RRDB_PSNR_x4.pth
 device = torch.device('cuda')  # if you want to run on CPU, change 'cuda' -> cpu
@@ -11,8 +11,9 @@ device = torch.device('cuda')  # if you want to run on CPU, change 'cuda' -> cpu
 
 test_img_folder = 'LR/*'
 
-model = arch.RRDBNet(3, 3, 64, 23, gc=32)
-model.load_state_dict(torch.load(model_path), strict=True)
+# model = arch.RRDBNet(3, 3, 64, 23, gc=32)
+# model.load_state_dict(torch.load(model_path), strict=True)
+model = arch.load_default_model()
 model.eval()
 model = model.to(device)
 
